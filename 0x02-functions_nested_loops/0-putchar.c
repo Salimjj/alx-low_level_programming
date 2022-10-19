5cab2689f4b2c1f2a0d1bc8d782303976b5d34a7
salimjj <salemaliyu1992@gmail.com>
2022-10-19 04:56:43 -0700

    prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * main - send a b c d e f g h to the output
+ *
+ * Return: noting
+ */
+
+int main(void)
+{
+	char a = '_';
+	char b = 'p';
+	char c = 'u';
+	char d = 't';
+	char e = 'c';
+	char f = 'h';
+	char g = 'a';
+	char h = 'r';
+
+	_putchar(a);
+	_putchar(b);
+	_putchar(c);
+	_putchar(d);
+	_putchar(e);
+	_putchar(f);
+	_putchar(g);
+	_putchar(h);
+	_putchar('\n');
+
+	return (0);
+}
+
+
