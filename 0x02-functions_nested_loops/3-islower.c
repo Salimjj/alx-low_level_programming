1c7592e7ba7e07944fa04eba8bc73b87b668e8cf
salimjj <salemaliyu1992@gmail.com>
2022-10-19 15:43:24 -0700

    checks for lowercase character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,68 @@
+#include "main.h"
+
+/**
+ * _islower - send alphabet to the output
+ * @c: character
+ *
+ * Return: number
+ */
+int _islower(int c)
+{
+	int num1 = '1';
+	int num2 = '0';
+
+	if (c == 'a')
+		return (num1);
+	else if (c == 'b')
+		return (num1);
+	else if (c == 'c')
+		return (num1);
+	else if (c == 'd')
+		return (num1);
+	else if (c == 'e')
+		return (num1);
+	else if (c == 'f')
+		return (num1);
+	else if (c == 'g')
+		return (num1);
+	else if (c == 'h')
+		return (num1);
+	else if (c == 'i')
+		return (num1);
+	else if (c == 'j')
+		return (num1);
+	else if (c == 'k')
+		return (num1);
+	else if (c == 'l')
+		return (num1);
+	else if (c == 'm')
+		return (num1);
+	else if (c == 'n')
+		return (num1);
+	else if (c == 'o')
+		return (num1);
+	else if (c == 'p')
+		return (num1);
+	else if (c == 'q')
+		return (num1);
+	else if (c == 'r')
+		return (num1);
+	else if (c == 's')
+		return (num1);
+	else if (c == 't')
+		return (num1);
+	else if (c == 'u')
+		return (num1);
+	else if (c == 'v')
+		return (num1);
+	else if (c == 'w')
+		return (num1);
+	else if (c == 'x')
+		return (num1);
+	else if (c == 'y')
+		return (num1);
+	else if (c == 'z')
+		return (num1);
+	else
+		return (num2);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,3 +1,4 @@
 0-putchar prints _putchar, followed by a new line
 1-alphabet.c function that prints the alphabet, in lowercase, followed by a new line
 2-print_alphabet_x10.c function that prints 10 times the alphabet, in lowercase, followed by a new line
+3-islower.c  function that checks for lowercase character.
